d2b2cdade1f9b0d6f29a8ac1b485eb683a470324
Tran Nhan <trannhan0329@gmail.com>
2026-03-29 18:33:59 +0700

    btvn buoi 1

--- bài 1.py
+++ bài 1.py
@@ -4,4 +4,13 @@ Created on Sun Mar 29 18:23:05 2026
 
 @author: GHC
 """
-
+#Không có gì ngoài điều kiện
+n = int(input())
+if n >= 85:
+    print("Gioi")
+elif n >= 70:
+    print("Kha")
+elif n >= 50:
+    print("Trung binh")
+else:
+    print("Yeu")
